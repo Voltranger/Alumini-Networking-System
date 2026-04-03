722251e3ee5b6baf8d65633b4bd978d8ef607bb9
Voltranger <pranav.101205mahajan@gmail.com>
2026-04-03 16:31:12 +0530

    dependencies added

---
+++ alumini-network/src/test/java/com/pict/alumini/AluminiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pict.alumini;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluminiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
